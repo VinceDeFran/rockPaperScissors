2dce1eeb9927980093cc4992982fc705c2d5ced9
VinceDeFran <189347931+VinceDeFran@users.noreply.github.com>
2025-02-17 14:49:43 -0500

    Wrote Code for Steps 2 thru 4 Created a prompt to get the Human Choice.  Added variables to keep score.  Wrote the logic to play one round. Reduced the random number.  Provided console logs to indicate the winner.

--- javaScript.js
+++ javaScript.js
@@ -1,16 +1,56 @@
  console.log("Hello, JS script!")
+
+let humanScore = 0;
+let computerScore = 0;
+let humanChoice = " ";
+let computerChoice = " ";
+let randomNumber = Math.floor(Math.random() * 99);  
  
 function getComputerChoice () {  
-let randomNumber = Math.floor(Math.random() * 999999999);  
-let computerChoice = "none";
+console.log (`The Human score is ${humanScore}.`);
+console.log (`The Computer score is ${computerScore}.`);
 
- if (randomNumber <= 333333332)  {
-    computerChoice = "rock";
-  } else if ( (randomNumber > 333333332) && (randomNumber <= 666666665) ) {
-    computerChoice = "paper";
+ if (randomNumber <= 32)  {
+    computerChoice = "Rock";
+  } else if ( (randomNumber > 32) && (randomNumber <= 65) ) {
+    computerChoice = "Paper";
   } else {
-    computerChoice = "scissors";
+    computerChoice = "Scissors";
   }
-return computerChoice
+return computerChoice;
 }
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+getComputerChoice();
+
+alert ("Please open the Console to play.");
+
+function getHumanChoice () {
+  humanChoice = prompt("Please enter Rock, Paper, or Scissors." );
+  return humanChoice;
+}
+//console.log(getHumanChoice());
+getHumanChoice();
+
+
+function singleRound (computerChoice, humanChoice, humanScore, computerScore) {
+if (computerChoice === humanChoice) {
+  console.log("It's a tie.");
+} else if ( (humanChoice==="Rock") && (computerChoice==="Scissors")) {
+  console.log("Human wins the round.");
+  humanScore = humanScore + 1;
+  console.log (`The Human score is ${humanScore}.`);
+} else if ( (humanChoice==="Paper") && (computerChoice==="Rock")) {
+  console.log("Human wins the round.");
+  humanScore = humanScore + 1;
+  console.log (`The Human score is ${humanScore}.`);
+} else if ( (humanChoice==="Scissors") && (computerChoice==="Paper")) {
+  console.log("Human wins the round.");
+  humanScore = humanScore + 1;
+  console.log (`The Human score is ${humanScore}.`);
+} else {
+  console.log("Computer wins the round.");
+  computerScore = computerScore + 1;
+  console.log (`The Computer's score is ${computerScore}.`);
+}
+}
+singleRound (computerChoice, humanChoice, humanScore, computerScore);
